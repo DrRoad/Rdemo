7957aca8eef702d54873b34e2e566fc03f1aadf2
miarangoa <miarangoa@unal.edu.co>
2017-09-29 19:04:28 -0500

    Fn gráficos residuales completa

--- R/residuals.reg.R
+++ R/residuals.reg.R
@@ -1,6 +1,7 @@
 library(dataseries)
 library(forecast)
 library(ggplot2)
+library(gridExtra)
 
 residuals.reg <- function(
   datos,
@@ -46,10 +47,50 @@ residuals.reg <- function(
     d <- data.frame(ti, r)
     p <- ggplot(d, aes(ti,r)) +
       geom_point() +
+      geom_hline(yintercept=0, lty=3) +
       ggtitle("Residuals") +
+      theme(plot.title = element_text(hjust = 0.5)) +
       xlab("ti") +
       ylab("Residuals")
     p <- p +geom_line(yintercept=0)+geom_smooth()
-    return(p)
+
+
+    s <- sqrt(deviance(model)/df.residual(model))
+    rs <- r/s
+
+    w <- qnorm(ppoints(rs))
+    d2 <- data.frame(w, rs=sort(rs))
+    p1 <- ggplot(d2, aes(w, rs)) +
+      geom_point() +
+      geom_line(aes(x=rs, y=rs), col='red') +
+      ggtitle("Normal Q-Q") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      xlab("Theoretical Quantiles") +
+      ylab("Sample Quantiles")
+
+
+    p2 <- ggplot(model, aes(x=r)) +
+      geom_density(fill="yellow", colour=NA, alpha=.2) +
+      geom_line(stat = "density") +
+      expand_limits(y = 0) +
+      ggtitle("Density") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      xlab("Residuals") +
+      ylab("Density")
+
+
+    ww <- acf(r, plot = FALSE)
+    www <- with(ww, data.frame(lag, acf))
+    significance_level <- qnorm((1 + 0.95)/2)/sqrt(sum(!is.na(r)))
+    p3 <- ggplot(data = www, mapping = aes(x = lag, y = acf)) +
+      geom_hline(aes(yintercept = 0)) +
+      geom_hline(yintercept=c(significance_level,-significance_level), lty=3, col='blue') +
+      geom_segment(mapping = aes(xend = lag, yend = 0)) +
+      ggtitle("ACF") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      xlab("Lag") +
+      ylab("ACF")
+
+    return(grid.arrange(p,p1,p2,p3, ncol=2))
 }
 
